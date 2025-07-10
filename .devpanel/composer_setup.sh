c3893e3228808970ddec2d5afc6d7646894e8ba6
Darren Oh <2293701+darrenoh@users.noreply.github.com>
2025-07-10 16:56:01 -0400

    Add Storybook

--- .devpanel/composer_setup.sh
+++ .devpanel/composer_setup.sh
@@ -16,3 +16,9 @@ composer config -jm extra.drupal-scaffold.file-mapping '{
 }'
 composer config scripts.post-drupal-scaffold-cmd \
     'cd web/sites/default && test -z "$(grep '\''include \$devpanel_settings;'\'' settings.php)" && patch -Np1 -r /dev/null < $APP_ROOT/.devpanel/drupal-settings.patch || :'
+
+# Compile Storybook.
+composer config -j scripts.post-update-cmd \
+    'command -v nvm > /dev/null || curl -so- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash' \
+    'export NVM_DIR="$HOME/.nvm" && [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" && cd web/themes/contrib/demo_design_system && nvm install && npm install && npm run build' \
+    'export NVM_DIR="$HOME/.nvm" && [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" && cd web/themes/contrib/demo_design_system/starshot_demo && nvm install && npm install && npm run build'
